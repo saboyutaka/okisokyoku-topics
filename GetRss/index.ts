cee8f2e632467dde91575ec330f8d6a19c8a01a8
saboyutaka <yutaka.paradigm.shift@gmail.com>
2022-10-08 06:34:03 +0000

    get Rss

---
+++ GetRss/index.ts
@@ -0,0 +1,67 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import https = require("https");
+import cheerio = require("cheerio");
+import moment = require("moment");
+import { toXML } from "jstoxml";
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const url = req.query.url || "https://www.ogb.go.jp/";
+  const axios = require("axios");
+  axios.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
+  const response = await axios.get(url);
+  const $ = cheerio.load(response.data);
+
+  const items = [];
+  $("#mainContentsWrapper table tr").map((i, tr) => {
+    const category = $(tr).find("th span").text();
+    const date = $(tr).find("th p.date").text();
+    const pubDate = moment(date, "YYYY年MM月DD日").format(
+      "ddd, DD MMM YYYY 10:mm:ss +0900"
+    );
+
+    const td1 = $(tr).find("td")[0];
+    const a = $(td1).find("a").first();
+    const link = a.attr("href");
+    const title = a.text().trim();
+
+    const td2 = $(tr).find("td")[1];
+    const department = $(td2).text().trim();
+
+    const description = "";
+
+    items.push({ title, description, link, pubDate, category, department });
+  });
+
+  const channel = [];
+  channel.push({ title: "内閣府・沖縄総合事務局 新着情報" });
+  channel.push({ link: "https://www.ogb.go.jp/news" });
+  items.forEach((item) => {
+    channel.push({ item: item });
+  });
+
+  const feed = {
+    rss: {
+      _attrs: {
+        version: "2.0",
+        "xmlns:dc": "http://purl.org/dc/elements/1.1/",
+        "xml:lang": "ja",
+      },
+      channel,
+    },
+  };
+
+  const rss = toXML(feed);
+
+  context.res = {
+    body: rss,
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  };
+};
+
+export default httpTrigger;
